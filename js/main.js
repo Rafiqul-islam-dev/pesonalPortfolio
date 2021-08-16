444946de1fe5a995d565359b3196339f9c29d0ef
Rafiqul-islam-dev <rafiqulislam93.bd@gmail.com>
2021-08-17 01:13:48 +0600

    service update

--- js/main.js
+++ js/main.js
@@ -1,22 +1,23 @@
 /*----------------About section------------------*/
-(() =>{
+(() => {
+  const aboutSection = document.querySelector(".about-section"),
+   tabsContainer = document.querySelector(".about-tabs");
 
-    const aboutSection = document.querySelector(".about-section"),
-    tabsContainer = document.querySelector(".about-tabs");
-
-    tabsContainer.addEventListener("click", (event) => {
-        if(event.target.classlist.contains("tab-item") && !event.target.classlist.contains("active")){
-            console.log("event.target contains 'tab-items' class and not contains 'active' class");
-            console.log(event.target);
-        }
-    })
-    //     if(event.target.classlist.contains("tab-item") && !event.target.classlist.contains("active")){
-    //         const target = event.target.getAttribute("data-target");
-    //         console.log(target);
-    //         // tabsContainer.querySelector(".active").classlist.remove("outer-shadow", "active");
-    //         // event.target.classlist.add("active", "outer-shadow");
-    //         // aboutSection.querySelector(".tab-content.active").classlist.remove("active");
-    //         // aboutSection.querySelector(target).classlist.add("active")
-    //     }
-    // })
+  tabsContainer.addEventListener("click", (event) => {
+    if (
+      event.target.classList.contains("tab-item") &&
+      !event.target.classList.contains("active")
+    ) {
+      const target = event.target.getAttribute("data-target");
+      tabsContainer.querySelector(".active").classList.remove("outer-shadow", "active");
+      //active new 'tab-item'
+      event.target.classList.add("active", "outer-shadow");
+      //deactive existing active
+      aboutSection.querySelector(".tab-content.active").classList.remove("active");
+      //active new 'tab-content'
+      aboutSection.querySelector(target).classList.add("active");
+      //console.log(target);
+    }
+  });
+  
 })();
