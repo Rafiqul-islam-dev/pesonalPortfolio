58940055ea0649de46b82adf8d97eb9ce5eacfea
Rafiqul-islam-dev <rafiqulislam93.bd@gmail.com>
2021-08-17 18:00:05 +0600

    update portfolio

--- js/main.js
+++ js/main.js
@@ -21,3 +21,51 @@
   });
   
 })();
+/*-----------------portfolio filter and popp----------------*/
+(()=>{
+    const filterContainer = document.querySelector(".portfolio-filter"),
+    portfolioItemsContainer = document.querySelector(".portfolio-items"),
+    portFolioItems = document.querySelectorAll(".portfolio-item"),
+    popup = document.querySelector(".portfolio-popup"),
+    prevBtn = popup.querySelector(".pp-prev"),
+    nextBtn = popup.querySelector(".pp-next"),
+    closeBtn =popup.querySelector(".pp-close"),
+    projectDetailsContainer = popup.querySelector(".pp-details"),
+    projectDetailsBtn = popup.querySelector(".pp-project-details-btn");
+
+    let intemIndex, slideIndex, screenshort;
+
+    /* filter portfolio items*/
+    filterContainer.addEventListener("click", (event)=>{
+        if(event.target.classList.contains("filter-item") && !event.target.classList.contains("active")){
+            //deactive active filter-item
+            filterContainer.querySelector(".active").classList.remove("outer-shadow", "active");
+
+            //active new filter-item
+            event.target.classList.add("active", "outer-shadow");
+            const target = event.target.getAttribute("data-target");
+            portFolioItems.forEach((item) =>{
+                if(target === item.getAttribute("data-catagory") || target === 'all'){
+                    item.classList.remove("hide");
+                    item.classList.add("show");
+                }
+                else{
+                    item.classList.remove("show");
+                    item.classList.add("hide");
+                }
+            })
+        }
+    })
+    portfolioItemsContainer.addEventListener("click", (event)=>{
+        if(event.target.closest(".portfolio-item-inner")){
+            const portFolioItems = event.target.closest(".portfolio-item-inner").parentElement;
+            
+            //get the portfolioitem index
+
+            intemIndex = Array.from(portFolioItems.parentElement.children).indexOf(portFolioItems);
+            //screenshort = portFolioItems[intemIndex].querySelector(".portfolio-item-img img").getAttribute("data-screenshort");
+           screenshort = portFolioItems[intemIndex].querySelector(".portfolio-item-img img").getAttribute("data-screenshorts");
+            console.log(screenshort);
+        }
+    })
+})()
